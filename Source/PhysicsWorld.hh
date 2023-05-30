53aada5cc4bdd73fc1e804d7bf011bfa1e9cb15f
Floyden <keisen95@gmail.comx>
2023-05-30 16:32:36 +0200

    Add nodiscard

--- Source/PhysicsWorld.hh
+++ Source/PhysicsWorld.hh
@@ -9,14 +9,15 @@ namespace Solis::Physics
 class World
 {
 public:
-    
     template <Derived<IShape> Shape, class... Args>
+    [[nodiscard]]
     Shape *CreateShape(Args &&...args)
     {
         auto *shape = mShapes.emplace_back(UPtr<IShape>(new Shape(std::forward<Args>(args)...))).get();
         return reinterpret_cast<Shape *>(shape);
     }
 
+    [[nodiscard]]
     RigidBody *CreateRigidBody(const Isometry &transform, IShape *shape)
     {
         return mRigidBodies.emplace_back(UPtr<RigidBody>(new RigidBody(transform, shape))).get();
