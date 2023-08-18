9ad8b38ae9feb1ab89ac6f43891a20d01fb3db4f
Floyden <keisen95@gmail.comx>
2023-08-18 12:33:34 +0200

    Modify the test main some

--- Source/Main.c
+++ Source/Main.c
@@ -4,9 +4,12 @@
 int main()
 {
     Sol_ShapeRectangle2D rect1 = {5.0, 5.0};
-    Sol_ShapeRectangle2D rect2 = {2.0, 2.0};
+    Sol_ShapeRectangle2D rect2 = {2.0, 4.0};
     Sol_Isometry2D isometry1 = Sol_ISOMETRY2D_IDENTITY;
     Sol_Isometry2D isometry2 = Sol_ISOMETRY2D_IDENTITY;
-    isometry2.translation.x = 5.0;
-    printf("Hi\n");
+    isometry2.translation = (Sol_Vec2){3.0, 2.0};
+    isometry2.rotation[0] = sqrt(2.0) / 2.0;
+    isometry2.rotation[1] = sqrt(2.0) / 2.0;
+    Sol_CollisionContactInfo2D colInfo;
+    Sol_CollisionCheckRectangleRectangle(&rect1, &rect2, &isometry2, &colInfo);
 }
