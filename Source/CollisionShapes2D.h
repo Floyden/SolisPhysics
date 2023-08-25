990196a5d2acd8993dfe50fa7208732b70e5ab2d
Floyden <floyden@fnaa.net>
2023-08-25 18:39:32 +0200

    Use header guards in collisionshapes2d

--- Source/CollisionShapes2D.h
+++ Source/CollisionShapes2D.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef SOL_COLLISION_SHAPES_2D
+#define SOL_COLLISION_SHAPES_2D
 #include "Defines.h"
 #include "Math.h"
 
@@ -79,3 +80,4 @@ int Sol_CollisionCheckCapsuleSegment(Sol_ShapeCapsule2D const* c, Sol_ShapeSegme
 int Sol_CollisionCheckCapsuleSphere(Sol_ShapeCapsule2D const* c, Sol_ShapeSphere2D const* s, Sol_Isometry2D const* difference, Sol_CollisionContactInfo2D* contactInfo);
 
 
+#endif SOL_COLLISION_SHAPES_2D
