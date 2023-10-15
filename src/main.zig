c9015efbc21e313e96356f2cbcc86ab40e9e34ec
Floyden <floyden@fnaa.net>
2023-10-15 21:52:28 +0200

    Add Forces

--- src/main.zig
+++ src/main.zig
@@ -5,23 +5,24 @@ const CollisionShape = CollisionShapes.CollisionShape;
 const Transform = @import("transform.zig").Transform2D;
 const Collider = @import("collider_2d.zig");
 const PhysicsWorld = @import("physics_world.zig").PhysicsWorld;
-
+const RigidBody = @import("physics_world.zig").RigidBody;
 
 pub fn main() !void {
     var world = PhysicsWorld.new();
     defer world.deinit();
 
-    const rect1 = CollisionShapes.Rectangle.new(1.0, 1.0);
-    const rect1Shape = CollisionShape{ .rectangle = rect1 };
-    const rect2 = CollisionShapes.Sphere{ .radius = 1.0 };
-    const rect2Shape = CollisionShape{ .sphere = rect2 };
+    const rect1Shape = CollisionShape{ .rectangle = CollisionShapes.Rectangle.new(1.0, 1.0) };
+    const rect2Shape = CollisionShape{ .sphere = CollisionShapes.Sphere{ .radius = 1.0 } };
     const transform = Transform.fromTranslation(Vec2.new(2.0, 0.0));
 
     var colliderArray = std.ArrayList(Collider.Collider2D).init(std.heap.page_allocator);
     defer colliderArray.deinit();
 
-    _ = world.addCollider(Collider.Collider2D{ .shape = rect1Shape, .transform = transform, .mass = 1.0 });
-    _ = world.addCollider(Collider.Collider2D{ .shape = rect2Shape, .transform = transform, .mass = 1.0 });
+    const c1 = world.addCollider(Collider.Collider2D{ .shape = rect1Shape, .transform = Transform.identity(), .mass = 1.0 });
+    const c2 = world.addCollider(Collider.Collider2D{ .shape = rect2Shape, .transform = transform, .mass = 1.0 });
+
+    _ = world.addRigidBody(RigidBody{ .colliderId = c1, .velocity = Vec2.zero(), .forces = Vec2.zero(), .mass = 1.0 });
+    _ = world.addRigidBody(RigidBody{ .colliderId = c2, .velocity = Vec2.zero(), .forces = Vec2.zero(), .mass = 1.0 });
 
     const res = Collider.detectCollisions(world.colliderList);
     defer res.deinit();
--- src/physics_world.zig
+++ src/physics_world.zig
@@ -7,16 +7,26 @@ pub const RigidBody = struct {
     velocity: Vec2,
     forces: Vec2,
     mass: f32,
+
+    pub inline fn applyForce(self: *RigidBody, force: Vec2) void {
+        self.forces.add(force);
+    }
+
+    pub inline fn resetForces(self: *RigidBody) void {
+        self.forces = Vec2.zero();
+    }
 };
 
 pub const PhysicsWorld = struct {
     colliderList: std.ArrayList(Collider.Collider2D),
     rigidBodyList: std.ArrayList(RigidBody),
+    gravity: Vec2,
 
     pub fn new() PhysicsWorld {
         return PhysicsWorld{
             .colliderList = std.ArrayList(Collider.Collider2D).init(std.heap.page_allocator),
             .rigidBodyList = std.ArrayList(RigidBody).init(std.heap.page_allocator),
+            .gravity = Vec2.new(0.0, 9.81),
         };
     }
 
@@ -33,5 +43,17 @@ pub const PhysicsWorld = struct {
     pub fn addRigidBody(self: *PhysicsWorld, collider: RigidBody) u64 {
         self.rigidBodyList.append(collider) catch unreachable;
         return self.rigidBodyList.items.len - 1;
+    }
+
+    pub fn resetForces(self: *PhysicsWorld) void {
+        for (self.rigidBodyList.item) |rb| {
+            rb.resetForces();
+        }
+    }
+
+    pub fn applyGravity(self: *PhysicsWorld) void {
+        for (self.rigidBodyList.item) |rb| {
+            rb.applyForce(self.gravity);
+        }
     }
 };
