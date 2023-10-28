613d1070a0b33c18e948daa7c1d84d75a0c919fd
Floyden <floyden@fnaa.net>
2023-10-29 01:49:17 +0200

    Add raylib for debugging

--- src/main.zig
+++ src/main.zig
@@ -6,14 +6,20 @@ const Transform = @import("transform.zig").Transform2D;
 const Collider = @import("collider_2d.zig");
 const PhysicsWorld = @import("physics_world.zig").PhysicsWorld;
 const RigidBody = @import("physics_world.zig").RigidBody;
+const ray = @cImport({
+    @cInclude("raylib.h");
+});
 
 pub fn main() !void {
+    ray.InitWindow(600, 480, "Test");
+    defer ray.CloseWindow();
+
     var world = PhysicsWorld.new();
     defer world.deinit();
 
-    const rect1Shape = CollisionShape{ .rectangle = CollisionShapes.Rectangle.new(1.0, 1.0) };
-    const rect2Shape = CollisionShape{ .sphere = CollisionShapes.Sphere{ .radius = 1.0 } };
-    const transform = Transform.fromTranslation(Vec2.new(2.0, 0.0));
+    const rect1Shape = CollisionShape{ .rectangle = CollisionShapes.Rectangle.new(100.0, 50.0) };
+    const rect2Shape = CollisionShape{ .sphere = CollisionShapes.Sphere{ .radius = 50.0 } };
+    const transform = Transform.fromTranslation(Vec2.new(200.0, 100.0));
 
     var colliderArray = std.ArrayList(Collider.Collider2D).init(std.heap.page_allocator);
     defer colliderArray.deinit();
@@ -23,6 +29,18 @@ pub fn main() !void {
 
     _ = world.addRigidBody(RigidBody{ .colliderId = c1, .velocity = Vec2.zero(), .forces = Vec2.zero(), .mass = 1.0 });
     _ = world.addRigidBody(RigidBody{ .colliderId = c2, .velocity = Vec2.zero(), .forces = Vec2.zero(), .mass = 1.0 });
+
+    while (!ray.WindowShouldClose()) {
+        ray.BeginDrawing();
+
+        const r = world.getCollider(c1);
+        const s = world.getCollider(c2);
+        ray.DrawCircle(@intFromFloat(s.*.transform.translation.x), @intFromFloat(s.*.transform.translation.y), s.*.shape.sphere.radius, ray.MAROON);
+        ray.DrawRectangle(@intFromFloat(r.*.transform.translation.x), @intFromFloat(r.*.transform.translation.y), @intFromFloat(r.*.shape.rectangle.halfWidth), @intFromFloat(r.*.shape.rectangle.halfWidth), ray.GREEN);
+
+        ray.EndDrawing();
+    }
+    world.step(16.0);
 
     const res = Collider.detectCollisions(world.colliderList);
     defer res.deinit();
