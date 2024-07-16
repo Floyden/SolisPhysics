bf9c597026c100c73ede490bcf00eb16dbe04248
Floyden <floyden@fnaa.net>
2024-07-16 22:07:35 +0200

    Add C callconv

--- src/ffi.zig
+++ src/ffi.zig
@@ -14,30 +14,30 @@ export fn Vec2_addMut(a: *Vec2, b: Vec2) callconv(.C) void {
     return a.addMut(b);
 }
 
-export fn PhysicsWorld_Create() *PhysicsWorld {
+export fn PhysicsWorld_Create() callconv(.C) *PhysicsWorld {
     const world = std.heap.page_allocator.create(PhysicsWorld) catch unreachable;
     world.* = PhysicsWorld.new();
 
     return world;
 }
 
-export fn PhysicsWorld_Destroy(world: *PhysicsWorld) void {
+export fn PhysicsWorld_Destroy(world: *PhysicsWorld) callconv(.C) void {
     world.deinit();
     std.heap.page_allocator.destroy(world);
 }
 
-export fn PhysicsWorld_Step(world: *PhysicsWorld, dt: f32) void {
+export fn PhysicsWorld_Step(world: *PhysicsWorld, dt: f32) callconv(.C) void {
     world.step(dt) catch unreachable;
 }
 
-export fn PhysicsWorld_CreateRectangleCollider(world: *PhysicsWorld, width: f32, height: f32, isometry: Isometry2) u64 {
+export fn PhysicsWorld_CreateRectangleCollider(world: *PhysicsWorld, width: f32, height: f32, isometry: Isometry2) callconv(.C) u64 {
     return world.addCollider(Collider2.new(CollisionShapes.newRectangle(width, height), isometry));
 }
 
-export fn Colliders_GetIsometry(world: *PhysicsWorld, collider: u64) *Isometry2 {
+export fn Colliders_GetIsometry(world: *PhysicsWorld, collider: u64) callconv(.C) *Isometry2 {
     return &world.getCollider(collider).transform;
 }
 
-export fn PhysicsWorld_CreateRigidBody(world: *PhysicsWorld, collider: u64, mass: f32) u64 {
+export fn PhysicsWorld_CreateRigidBody(world: *PhysicsWorld, collider: u64, mass: f32) callconv(.C) u64 {
     return world.addRigidBody(RigidBody.new(collider, mass));
 }
